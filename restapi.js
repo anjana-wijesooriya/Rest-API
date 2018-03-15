d9de494c027fcaf8d9ac273fa06847f22061e894
anjanagihan666 <ajgihan@gmail.com>
2018-03-15 16:45:37 +0530

    test commit

--- restapi.js
+++ restapi.js
@@ -13,6 +13,13 @@ var restapi = express();
 
 restapi.use(express.static(__dirname + '/public'));
 
+db.all("/api/*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization,   Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    return next();
+});
+
 
 restapi.get('/data', function (request, response) {
     db.all("SELECT * FROM counts", function (error, rows) {
